d9889d56e78c2e7b12db43a6751729775735cf19
k0rsakov <korsakov.iyu@gmail.com>
2025-07-16 14:57:49 +0300

    Изменён проект pet_project_s3_as_standard: - Исправлены демо-скрипты - Добавлены демо-скрипты

--- main.py
+++
@@ -1,6 +0,0 @@
-from s3_utils import S3_CONFIGS, boto3_list_buckets, minio_list_buckets
-
-# Например, для MinIO:
-minio_list_buckets(S3_CONFIGS["aws"])
-boto3_list_buckets(S3_CONFIGS["aws"])  # Работает и так!
-
