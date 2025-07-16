fff99ce9dc5f4347d35b03a55701a5c42de49b96
k0rsakov <korsakov.iyu@gmail.com>
2025-07-16 06:29:49 +0300

    Изменён проект pet_project_s3_as_standard: - Добавлены примеры ключей

--- cred_example.py
+++ cred_example.py
@@ -7,3 +7,13 @@ s3_selectel_bucket_name=""
 s3_selectel_endpoint="s3.ru-7.storage.selcloud.ru"
 s3_selectel_access=""
 s3_selectel_secret=""
+
+s3_minio_bucket_name=""
+s3_minio_endpoint="localhost:9000"
+s3_minio_access_key=""
+s3_minio_secret_key=""
+
+s3_aws_bucket_name=""
+s3_aws_endpoint="s3.amazonaws.com"
+s3_aws_access_key=""
+s3_aws_secret_key=""
