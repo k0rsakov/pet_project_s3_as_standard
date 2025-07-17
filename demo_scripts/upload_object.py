3781c8bb0aecc9aef251dfed7a87d34a6fb37783
k0rsakov <korsakov.iyu@gmail.com>
2025-07-17 15:51:49 +0300

    Изменён проект pet_project_s3_as_standard: - Проверка и дебаг

--- demo_scripts/upload_object.py
+++ demo_scripts/upload_object.py
@@ -4,50 +4,50 @@ minio_upload_csv(
     conn_params=S3_CONFIGS["minio"],
     bucket_name=S3_CONFIGS["minio"]["bucket"],
     object_name="titanic_minio_client.csv",
-    file_path="./titanic.csv",
+    file_path="../titanic.csv",
 )
 boto3_upload_csv(
     conn_params=S3_CONFIGS["minio"],
     bucket_name=S3_CONFIGS["minio"]["bucket"],
     object_name="titanic_boto3_client.csv",
-    file_path="./titanic.csv",
+    file_path="../titanic.csv",
 )
 
 minio_upload_csv(
     conn_params=S3_CONFIGS["selectel"],
     bucket_name=S3_CONFIGS["selectel"]["bucket"],
     object_name="titanic_minio_client.csv",
-    file_path="./titanic.csv",
+    file_path="../titanic.csv",
 )
 boto3_upload_csv(
     conn_params=S3_CONFIGS["selectel"],
     bucket_name=S3_CONFIGS["selectel"]["bucket"],
     object_name="titanic_boto3_client.csv",
-    file_path="./titanic.csv",
+    file_path="../titanic.csv",
 )
 
 minio_upload_csv(
     conn_params=S3_CONFIGS["vk"],
     bucket_name=S3_CONFIGS["vk"]["bucket"],
     object_name="titanic_minio_client.csv",
-    file_path="./titanic.csv",
+    file_path="../titanic.csv",
 )
 boto3_upload_csv(
     conn_params=S3_CONFIGS["vk"],
     bucket_name=S3_CONFIGS["vk"]["bucket"],
     object_name="titanic_boto3_client.csv",
-    file_path="./titanic.csv",
+    file_path="../titanic.csv",
 )
 
 minio_upload_csv(
     conn_params=S3_CONFIGS["aws"],
     bucket_name=S3_CONFIGS["aws"]["bucket"],
     object_name="titanic_minio_client.csv",
-    file_path="./titanic.csv",
+    file_path="../titanic.csv",
 )
 boto3_upload_csv(
     conn_params=S3_CONFIGS["aws"],
     bucket_name=S3_CONFIGS["aws"]["bucket"],
     object_name="titanic_boto3_client.csv",
-    file_path="./titanic.csv",
+    file_path="../titanic.csv",
 )
